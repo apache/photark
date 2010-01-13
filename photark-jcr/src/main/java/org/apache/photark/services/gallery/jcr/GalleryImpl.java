7b4213cbee3f8d5ce6b55656b125fa1bcbeee9b4
Luciano Resende <lresende@apache.org>
2010-01-13 16:48:00 +0000

    PHOTARK-9 - Adding other files I missed from Avdhesh Yadav patch
    git-svn-id: https://svn.apache.org/repos/asf/incubator/photark/trunk@898834 13f79535-47bb-0310-9956-ffa450edef68

---
+++ photark-jcr/src/main/java/org/apache/photark/services/gallery/jcr/GalleryImpl.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.photark.services.gallery.jcr;
+
+import java.io.File;
+import java.net.URL;
+
+import org.apache.photark.services.album.Album;
+import org.apache.photark.services.album.jcr.AlbumImpl;
+import org.apache.photark.services.gallery.AbsGalleryImpl;
+import org.apache.photark.services.gallery.Gallery;
+import org.oasisopen.sca.annotation.Init;
+
+public class GalleryImpl extends AbsGalleryImpl implements Gallery{
+    
+    public GalleryImpl() {
+    	
+    }
+    
+    public GalleryImpl(String name) {
+        super(name);
+    }
+    
+    @Init
+    public void init() {
+    	System.out.println(">>> Initializing JCR Gallery");
+        try {
+            URL galleryURL = this.getClass().getClassLoader().getResource(name);
+            if(galleryURL == null) {
+                // Accomodate for J2EE classpath that starts in WEB-INF\classes
+                galleryURL = this.getClass().getClassLoader().getResource("../../" + name);
+            }
+
+            if(galleryURL != null) {
+                File album = new File(galleryURL.toURI());
+                if (album.isDirectory() && album.exists()) {
+                    File[] albums = album.listFiles();
+                    for(File albumFile : albums) {
+                        if(! albumFile.getName().startsWith(".")) {
+                            if(albumFile.isDirectory() && albumFile.exists()) {
+                                Album newAlbum = AlbumImpl.createAlbum(albumFile.getName());
+                                newAlbum.setName(albumFile.getName());
+                                ((AlbumImpl)newAlbum).setGallery(name);
+                                this.albums.add(newAlbum);
+                            }                                
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            // FIXME: ignore for now
+            e.printStackTrace();
+        }
+        
+        initialized = true;
+    }
+}
---
+++ photark/src/main/java/org/apache/photark/services/gallery/AbsGalleryImpl.java
@@ -0,0 +1,105 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.photark.services.gallery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.photark.services.album.Album;
+import org.oasisopen.sca.annotation.Property;
+
+public abstract class AbsGalleryImpl implements Gallery {
+	
+	protected String name;
+	private String location;
+	protected boolean initialized;
+	protected List<Album> albums = new ArrayList<Album>();
+
+	public AbsGalleryImpl() {
+
+	}
+
+	public AbsGalleryImpl(String name) {
+		this.name = name;
+	}
+
+	public abstract void init(); 
+
+	public String getName() {
+		return name;
+	}
+
+	@Property
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void addAlbum(Album album) {
+
+	}
+
+	public Album[] getAlbums() {
+		if(! initialized) {
+			init();
+		}
+
+		Album[] albumArray = new Album[albums.size()];
+		albums.toArray(albumArray);
+		return albumArray;
+	}
+
+	public String getAlbumCover(String albumName) {
+		Album albumLookup = getAlbum(albumName);
+
+		if (albumLookup != null) {
+			return albumLookup.getPictures()[0];
+		} else {
+			//FIXME: return proper not found exception
+			return null;             
+		}
+	}
+
+	public String[] getAlbumPictures(String albumName) {
+		Album albumLookup = getAlbum(albumName);
+
+		if (albumLookup != null) {
+			return albumLookup.getPictures();
+		} else {
+			//FIXME: return proper not found exception
+			return new String[]{};             
+		}
+	}
+
+	private Album getAlbum(String albumName) {
+		Album albumLookup = null;
+		for(Album album : albums) {
+			if(album.getName().equalsIgnoreCase(albumName)) {
+				albumLookup = album;
+				break;
+			}
+		}
+
+		return albumLookup;
+	}
+
+	private String getLocation() {
+		return location;
+	}
+}
