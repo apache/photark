7df24cf522c0cd6e76ad814877476f61eff7050c
Avdhesh Yadav <avd@apache.org>
2010-05-01 08:12:53 +0000

    Fixing test cases to avoid issues when album cover picture read in different order.Also fixed the missing apache Licenses
    git-svn-id: https://svn.apache.org/repos/asf/incubator/photark/trunk@939943 13f79535-47bb-0310-9956-ffa450edef68

--- photark-ui/src/test/java/org/apache/photark/ui/IE8TestBrowser.java
+++ photark-ui/src/test/java/org/apache/photark/ui/IE8TestBrowser.java
@@ -1,3 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.    
+ */
+
 package org.apache.photark.ui;
 
 import static org.junit.Assert.assertFalse;
@@ -23,8 +42,6 @@ import com.gargoylesoftware.htmlunit.html.HtmlOption;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 import com.gargoylesoftware.htmlunit.html.HtmlTable;
-
-
 
 
 public class IE8TestBrowser {
@@ -96,7 +113,7 @@ public class IE8TestBrowser {
 		Thread.sleep(3000);
 		//testing whether the expected changes has happened
 		assertTrue(select.asText().contains("boston"));
-		assertTrue(page.getHtmlElementById("albumCover").asXml().contains("photark/gallery/boston/dsc00376.jpg"));
+		assertTrue(page.getHtmlElementById("albumCover").asXml().contains("photark/gallery/boston/dsc"));
 		
 		assertTrue(page.getHtmlElementById("adminTableGallery").asXml().contains("photark/gallery/boston/dsc00376.jpg"));
 		assertTrue(page.getHtmlElementById("adminTableGallery").asXml().contains("photark/gallery/boston/dsc00368.jpg"));
