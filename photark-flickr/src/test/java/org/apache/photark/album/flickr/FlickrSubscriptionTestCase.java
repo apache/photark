0d9428985cf0dd881534d8d08bd2d0726ce80d62
Luciano Resende <lresende@apache.org>
2010-08-14 17:36:41 +0000

    PHOTARK-19 - Applying patch from Luis Fernando enhancing support for reading Flickr photo stream
    git-svn-id: https://svn.apache.org/repos/asf/incubator/photark/branches/photark-rest@985541 13f79535-47bb-0310-9956-ffa450edef68

--- photark-flickr/src/test/java/org/apache/photark/album/flickr/FlickrSubscriptionTestCase.java
+++ photark-flickr/src/test/java/org/apache/photark/album/flickr/FlickrSubscriptionTestCase.java
@@ -6,15 +6,15 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
- * under the License.    
+ * under the License.
  */
 
 package org.apache.photark.album.flickr;
@@ -25,11 +25,12 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class FlickrSubscriptionTestCase {
+    private static final String SUBSCRIPTION_URL = "http://api.flickr.com/services/feeds/photos_public.gne?id=24662369@N07&lang=en-us&format=atom";
     private static FlickrPhotoStream photoStream;
 
     @BeforeClass
     public static void BeforeClass() {
-        photoStream = new FlickrPhotoStream("http://api.flickr.com/services/feeds/photos_public.gne?id=24662369@N07&lang=en-us&format=atom");
+        photoStream = new FlickrPhotoStream(SUBSCRIPTION_URL);
     }
 
     @Test
@@ -37,7 +38,12 @@ public class FlickrSubscriptionTestCase {
         Entry<String, Image>[] images = photoStream.getAll();
 
         for(Entry<String, Image> image : images) {
-            System.out.println(">>>" + image.getData().getName());
+            Image i = image.getData();
+
+            System.out.println(">>>>>>>>>>>>>");
+            System.out.println(">> ID       : " + i.getId());
+            System.out.println(">> Title    : " + i.getTitle());
+            System.out.println(">> Location : " + i.getLocation());
         }
     }
 }
