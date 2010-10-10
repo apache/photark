76faceb57937fbd1add6565ba3c8053546642684
Luciano Resende <lresende@apache.org>
2010-10-10 12:52:48 +0000

    Fixing test case to order albums to allow new album name generation to work properly
    git-svn-id: https://svn.apache.org/repos/asf/incubator/photark/branches/photark-rest@1006286 13f79535-47bb-0310-9956-ffa450edef68

--- photark-filesystem/src/test/java/org/apache/photark/services/filesystem/FileSystemGalleryTestCase.java
+++ photark-filesystem/src/test/java/org/apache/photark/services/filesystem/FileSystemGalleryTestCase.java
@@ -21,6 +21,9 @@ package org.apache.photark.services.filesystem;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import junit.framework.Assert;
 
@@ -43,12 +46,12 @@ import com.meterware.httpunit.WebResponse;
 
 /**
  * Verify various operations for the FileSystem Gallery
- * 
+ *
  * @version $Rev$ $Date$
  */
 public class FileSystemGalleryTestCase {
     private static final String GALLERY_SERVICE_URL = "http://localhost:8085/gallery";
-    
+
     private static Node node;
 
     @BeforeClass
@@ -61,93 +64,94 @@ public class FileSystemGalleryTestCase {
             e.printStackTrace();
         }
     }
-    
+
     @AfterClass
     public static void AfterClass() {
         node.stop();
     }
-    
+
     @Test
     public void testGetAlbums() throws IOException, JSONException {
-        JSONArray albums = readAlbums();
+        String[] albums = readAlbums();
 
-        //for debug purposes
-        //System.out.println(">>>" + jsonResponse.toString());    
-        
         Assert.assertNotNull(albums);
-        
     }
 
     @Test
     public void testAddAlbums() throws IOException, JSONException {
-        JSONArray albums = readAlbums();
-        int albumSize = albums.length();
-        
-        //for debug purposes
-        //System.out.println(">>>" + jsonResponse.toString());    
-        
+        String[] albums = readAlbums();
+        int albumSize = albums.length;
+
         addAlbum();
 
         albums = readAlbums();
 
-        //for debug purposes
-        //System.out.println(">>>" + jsonResponse.toString());    
-        
-        Assert.assertEquals(albumSize + 1, albums.length());
+        Assert.assertEquals(albumSize + 1, albums.length);
     }
-    
+
     @Test
     public void testRemoveAlbums() throws IOException, JSONException {
         WebConversation wc = new WebConversation();
         WebRequest request = new GetMethodWebRequest(GALLERY_SERVICE_URL + "/" + getLastAlbumName());
         ((GetMethodWebRequest) request).setMethod("DELETE");
         WebResponse response = wc.getResource(request);
-        
+
         Assert.assertEquals(200, response.getResponseCode());
     }
 
 
-    private JSONArray readAlbums()  throws IOException, JSONException {
+    private void addAlbum() throws IOException, JSONException {
+        JSONObject jsonAlbum = new JSONObject();
+        jsonAlbum.put("name", getNewAlbumName());
+        jsonAlbum.put("location", "http://localhost:8080/gallery/album/Boston");
+        jsonAlbum.put("description", "Some description goes here");
+
+        WebConversation wc = new WebConversation();
+        WebRequest request   = new PostMethodWebRequest(GALLERY_SERVICE_URL, new ByteArrayInputStream(jsonAlbum.toString().getBytes("UTF-8")),"application/json");
+        request.setHeaderField("Content-Type", "application/json");
+        WebResponse response = wc.getResource(request);
+
+        Assert.assertEquals(204, response.getResponseCode());
+    }
+
+    private String getNewAlbumName() throws IOException, JSONException {
+        String[] albums = readAlbums();
+        String album = albums[albums.length -1 ];
+        String[] tokens = album.split("-");
+        album = tokens[0] + "-" + (Integer.parseInt(tokens[1]) + 1);
+
+        return album;
+    }
+
+    private String getLastAlbumName() throws IOException, JSONException {
+        String[] albums = readAlbums();
+        String album = albums[albums.length -1];
+
+        return album;
+    }
+
+    private String[] readAlbums()  throws IOException, JSONException {
         WebConversation wc = new WebConversation();
         WebRequest request = new GetMethodWebRequest(GALLERY_SERVICE_URL);
         WebResponse response = wc.getResource(request);
 
         JSONObject jsonResponse = new JSONObject(response.getText());
-        JSONArray albums = (org.json.JSONArray) jsonResponse.get("albums");
 
         //for debug purposes
-        //System.out.println(">>>" + jsonResponse.toString());    
-        
-        return albums;
+        System.out.println(">>>" + jsonResponse.toString());
+
+        JSONArray albums = (org.json.JSONArray) jsonResponse.get("albums");
+        List<String> albumNames = new ArrayList<String>();
+        for(int pos=0; pos<albums.length(); pos++) {
+            JSONObject album = (JSONObject) albums.get(pos);
+            albumNames.add( album.getString("name"));
+        }
+
+        String[] albumNameArray = new String[albumNames.size()];
+        albumNames.toArray(albumNameArray);
+        Arrays.sort(albumNameArray, String.CASE_INSENSITIVE_ORDER);
+
+        return albumNameArray;
     }
-    
-    private void addAlbum() throws IOException, JSONException {
-        JSONObject jsonAlbum = new JSONObject();
-        jsonAlbum.put("name", getNewAlbumName());
-        
-        WebConversation wc = new WebConversation();
-        WebRequest request   = new PostMethodWebRequest(GALLERY_SERVICE_URL, new ByteArrayInputStream(jsonAlbum.toString().getBytes("UTF-8")),"application/json");
-        request.setHeaderField("Content-Type", "application/json");
-        WebResponse response = wc.getResource(request);
-        
-        Assert.assertEquals(204, response.getResponseCode());
-    }
-    
-    private String getNewAlbumName() throws IOException, JSONException {
-        JSONArray albums = readAlbums();
-        JSONObject album = (JSONObject) albums.get(albums.length() -1);
-        String albumName = album.getString("name");
-        String[] tokens = albumName.split("-");
-        albumName = tokens[0] + "-" + (Integer.parseInt(tokens[1]) + 1);
-        
-        return albumName;
-    }
-    
-    private String getLastAlbumName() throws IOException, JSONException {
-        JSONArray albums = readAlbums();
-        JSONObject album = (JSONObject) albums.get(albums.length() -1);
-        String albumName = album.getString("name");
-        
-        return albumName;
-    }
+
 }
